585d54c447531d564b844e21075bd210190d4182
Duarte <jose.duarte@roraimaenergia.com.br>
2023-06-12 12:00:00 -0400

    chore: change app folder structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+export class NewBootStrap {
+  async start() {
+    const app = await NestFactory.create(AppModule);
+    return app.listen(3000);
+  }
 }
-bootstrap();
+
+const bootStrap = new NewBootStrap();
+bootStrap.start();
